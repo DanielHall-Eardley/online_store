93d945536083f4593626cbe813bef56e831e76fe
DanielHall-Eardley <srt861hemicharger@gmail.com>
2019-01-03 22:20:13 -0500

    second commit 03/01/2018

--- src/components/SlideShow.vue
+++ src/components/SlideShow.vue
@@ -1,9 +1,13 @@
 <template>
     <transition>
-        <div style="{backgroundImage: url(@/assets/gto.jpg)}">
-            <h2>{{picTitle}}</h2>
-            <button id="left-button">Left</button>
-            <button id=right-button>Right</button>
+        <div id="slideshow-container" :style="{backgroundImage: `url(${url})`}">
+            <h2 id="pic-title">{{picTitle}}</h2>
+            <div id="slideshow-position">
+                <div v-bind:class="{highlighted: slideshowPosition(index)}" class="slideshow-indicator" v-for="(picture, index) in pictureArray">
+                </div>
+            </div>
+            <button name="left" v-on:click="scrollFunction()" id="left-button">Left</button>
+            <button name="right" v-on:click="scrollFunction()" id=right-button>Right</button>
         </div>
     </transition>
 </template>
@@ -27,37 +31,76 @@ export default {
                     url:"gto.jpg"
                 }
             ],
-                picTitle:"",
-                url:""
+                picTitle:"stuff",
+                url: require("@/assets/blue-camaro.png"),
+                counter: 1,
+
         }
     },
     beforeMount: function(){
         let array = this.pictureArray
-        let counter = 0
-        let id;
-            id = setInterval(()=>{
-                if(counter < array.length){
-                    this.picTitle = array[counter].title
-                    this.url = array[counter].url
-                    counter++
-                }else if(counter >= array.length){
+        let id = setInterval(()=>{
+                if(this.counter < array.length){
+                    this.picTitle = array[this.counter].title
+                    this.url = require(`@/assets/${array[this.counter].url}`)
+                    this.counter++
+                }else if(this.counter >= array.length){
                     clearInterval(id)
                 }
-            }, 5000, array, counter)
+            }, 5000, array, this.counter)
+        },
+    methods:{
+        slideshowPosition: function(index){
+           if(index + 1 === this.counter){
+               return true
+           }
+           else{
+               return false
+           }
+        },
+        scrollFunction: function(e){
+            console.log(e.target.id)
+            let array = this.pictureArray
         }
+    }
     
 }
 </script>
 
-<style scoped>
-    div{
+<style>
+    #slideshow-container{
         height: 100%;
         width: 100%;
+        background-size: 100%;
+        background-repeat: no-repeat;
+        position: relative;
     }
-    h2{
-
+    #pic-title{
+       text-align: center;
     }
-    img{
-        object-fit: cover;
+    #slideshow-position{
+        display: flex;
+        justify-content: center;
+        height: 10%;
+    }
+    .slideshow-indicator{
+        width: 3%;
+        height: 15%;
+        background-color:aqua;
+        margin: 0% 1%;
+        opacity: 0.2;
+    }
+    .highlighted{
+        opacity: 1;
+    }
+    #left-button{
+        position: absolute;
+        left: 1%;
+        top: 45%
+    }
+    #right-button{
+       position: absolute;
+       right: 1%;
+       top: 45%;
     }
 </style>
